d2d2c50cb639c58f972da9708023f81c4a91f1c1
Cyrill Augustiny <c.augustiny@gmail.com>
2024-08-30 14:00:36 +0200

    Worksheet_3

--- Worksheet_2/functions_compositions.js
+++ Worksheet_2/functions_compositions.js
@@ -1,20 +1,20 @@
 const rambda = require("../ramda.min.js");
 const sentence = 'PechaKucha is a presentation style in which 20 slides are shown for 20 seconds each (6 minutes and 40 seconds in total).';
 
-const splittedSentence = rambda.compose(rambda.split);
-const splittedSentenceArray = splittedSentence("", sentence);
+// const splittedSentence = rambda.compose(rambda.split);
+// const splittedSentenceArray = splittedSentence("", sentence);
 
-function countNumbers(splittedSentence) {
-    let i = 0;
-    for(splittedValue of splittedSentence) {
-        if(!isNaN(splittedValue) && splittedValue !== " ") {
-            i++;
-        }
-    }
-    return i;
-}
+// function countNumbers(splittedSentence) {
+//     let i = 0;
+//     for(splittedValue of splittedSentence) {
+//         if(!isNaN(splittedValue) && splittedValue !== " ") {
+//             i++;
+//         }
+//     }
+//     return i;
+// }
 
-console.log(countNumbers(splittedSentenceArray))
+// console.log(countNumbers(splittedSentenceArray))
 
 // const groupSentence = (groups, sentence) => {
 //     const {number = 0, notNumber = 0} = groups;
@@ -27,3 +27,16 @@ console.log(countNumbers(splittedSentenceArray))
 
 // const groupedSentenceValues = splittedSentence.reduce(splittedSentenceArray, {});
 // console.log(groupedSentenceValues);
+
+const isDigit = (char) => !isNaN(char) && char !== " ";
+const filterDigits = rambda.filter(isDigit);
+const countElements = (arr) => arr.length;
+
+const countNumbers = rambda.compose(
+    countElements,
+    filterDigits,
+    rambda.split("")
+);
+
+const result = countNumbers(sentence);
+console.log(result);
